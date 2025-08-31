84b0e7dff66276ca000632c742ca8407be8924e8
bearxiao9756 <bearxiao9@gmail.com>
2025-08-31 18:29:37 +0800

    Update index.tsx

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -8,7 +8,8 @@ import  { LoginModule } from '@tsdaodao/login';
 import  { DataSourceModule } from '@tsdaodao/datasource';
 import {ContactsModule} from '@tsdaodao/contacts';
 
-const apiURL = "https://api.botgate.cn/v1/"
+// const apiURL = "https://api.botgate.cn/v1/"
+const apiURL = "http://172.16.9.135:8090/v1/"
 
 if((window as any).__TAURI_IPC__) { // tauri环境
   console.log("tauri环境")
