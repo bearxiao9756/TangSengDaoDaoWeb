11ede82215c4963794ce8f7d6b8de4986ec2e71f
bearxiao9756 <bearxiao9@gmail.com>
2025-10-01 12:31:26 +0800

    111

--- apps/web/src-election/main/confing.ts
+++ apps/web/src-election/main/confing.ts
@@ -1,7 +1,8 @@
 const TSDD_FONFIG = {
   appId: "com.tsdaodao.im",
   name: "唐僧叨叨",
-  updataUrl: 'https://43.160.247.125/'
+  // updataUrl: 'https://43.160.248.114/'
+  updataUrl: 'https://43.160.248.114/v1'
 };
 
 export default TSDD_FONFIG;
