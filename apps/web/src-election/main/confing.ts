d214ab8caff468bb24e2677ea262988f10846528
bearxiao9756 <bearxiao9@gmail.com>
2025-10-01 13:19:02 +0800

    ip更换

--- apps/web/src-election/main/confing.ts
+++ apps/web/src-election/main/confing.ts
@@ -1,7 +1,7 @@
 const TSDD_FONFIG = {
   appId: "com.tsdaodao.im",
   name: "唐僧叨叨",
-  updataUrl: 'https://43.160.247.125/'
+  updataUrl: 'https://43.160.248.114/'
 };
 
 export default TSDD_FONFIG;
