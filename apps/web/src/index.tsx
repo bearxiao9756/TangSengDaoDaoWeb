fc3076c3e780efc23d6ad55940a958f9e1262792
bearxiao9756 <bearxiao9@gmail.com>
2025-10-03 22:11:07 +0800

    更换域名

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -8,7 +8,7 @@ import  { LoginModule } from '@tsdaodao/login';
 import  { DataSourceModule } from '@tsdaodao/datasource';
 import {ContactsModule} from '@tsdaodao/contacts';
 
-const apiURL = "https://api.botgate.cn/v1/"
+const apiURL = "https://hya2s2dhjk23.icu/v1/"
 
 if((window as any).__TAURI_IPC__) { // tauri环境
   console.log("tauri环境")
