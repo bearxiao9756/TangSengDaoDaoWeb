c8b1d6050fbeb0b45ce85d9e4b3fd750c40a2f3f
bearxiao9756 <bearxiao9@gmail.com>
2025-10-03 19:01:55 +0800

    ip 更换

--- apps/web/src-election/main/confing.ts
+++ apps/web/src-election/main/confing.ts
@@ -1,7 +1,7 @@
 const TSDD_FONFIG = {
   appId: "com.tsdaodao.im",
   name: "唐僧叨叨",
-  updataUrl: 'http://43.160.248.114:8090/'
+  updataUrl: 'http://43.160.244.68:8090/'
 };
 
 export default TSDD_FONFIG;
--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -9,7 +9,7 @@ import  { DataSourceModule } from '@tsdaodao/datasource';
 import {ContactsModule} from '@tsdaodao/contacts';
 
 // const apiURL = "https://api.botgate.cn/v1/"
-const apiURL = "http://43.160.248.114:8090/v1/"
+const apiURL = "http://43.160.244.68:8090/v1/"
 
 if((window as any).__TAURI_IPC__) { // tauri环境
   console.log("tauri环境")
