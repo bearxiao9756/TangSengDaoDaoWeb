bd7fe22e4a5b1e364b2cec690743a80734854a02
bearxiao9756 <bearxiao9@gmail.com>
2025-10-31 09:39:42 +0800

    换名

--- apps/web/electron-builder.js
+++ apps/web/electron-builder.js
@@ -69,7 +69,7 @@ module.exports = {
     // installerHeaderIcon: "./build/icon.ico", // 安装时头部图标
     createDesktopShortcut: true, // 创建桌面图标
     createStartMenuShortcut: true, // 创建开始菜单图标
-    shortcutName: "辰韬咨询", // 图标名称
+    shortcutName: "坤元服务", // 图标名称
   },
   linux: {
     target: ["AppImage", "deb"],
