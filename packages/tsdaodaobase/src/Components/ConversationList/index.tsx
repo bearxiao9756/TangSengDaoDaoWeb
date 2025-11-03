bc467cf371ae30f90be19e09accf63186e1a2bea
bearxiao9756 <bearxiao9@gmail.com>
2025-11-03 16:48:10 +0800

    Update index.tsx

--- packages/tsdaodaobase/src/Components/ConversationList/index.tsx
+++ packages/tsdaodaobase/src/Components/ConversationList/index.tsx
@@ -359,10 +359,10 @@ export default class ConversationList extends Component<
     console.log(channelInfo.orgData.displayName);
     console.log(channelInfo.channel.channelID);
     console.log(channelInfo.channel.channelType);
-    if(channelInfo.channel.channelType != 1){
+    if(channelInfo.channel.channelType == 1){
         Toast.error("不支持群组修改备注")
     }else{
-       WKApp.shared.baseContext.showUserInfo(channelInfo.channel.channelID, channelInfo.channel)
+       WKApp.shared.baseContext.showUserInfo(channelInfo.channel.channelID)
     }
   }
   onMute(channelInfo: ChannelInfo) {
