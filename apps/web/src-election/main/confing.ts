ac7aed9d75ca435a5f47aac66ca70c16f36f23e0
bearxiao9756 <bearxiao9@gmail.com>
2025-11-08 20:51:45 +0800

    更新域名123edu

--- apps/web/src-election/main/confing.ts
+++ apps/web/src-election/main/confing.ts
@@ -1,7 +1,7 @@
 const TSDD_FONFIG = {
   appId: "com.tsdaodao.im",
   name: "坤元服务",
-  updataUrl: 'https://123line.icu/api/'
+  updataUrl: 'https://123edu.icu/api/'
 };
 
 export default TSDD_FONFIG;
