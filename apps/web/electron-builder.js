f2daefb32035d95fcecf93131414fa127819427e
bearxiao9756 <bearxiao9@gmail.com>
2025-11-08 20:52:58 +0800

    更新

--- apps/web/electron-builder.js
+++ apps/web/electron-builder.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   publish: [{
     provider: "generic",
-    url: "https://123line.icu/v1/common/pcupdater/"
+    url: "https://123edu.icu/v1/common/pcupdater/"
   }],
   mac: {
     extendInfo: {
--- apps/web/src-election/main/confing.ts
+++ apps/web/src-election/main/confing.ts
@@ -1,7 +1,7 @@
 const TSDD_FONFIG = {
   appId: "com.tsdaodao.im",
   name: "坤元服务",
-  updataUrl: 'https://123line.icu/'
+  updataUrl: 'https://123edu.icu/'
 };
 
 export default TSDD_FONFIG;
