4f337fdad3366f015bc1b94628adfe9d3f035db5
bearxiao9756 <bearxiao9@gmail.com>
2025-11-10 09:15:31 +0800

    更新域名

--- apps/web/electron-builder.js
+++ apps/web/electron-builder.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   publish: [{
     provider: "generic",
-    url: "https://abhao.icu/v1/common/pcupdater/"
+    url: "https://bao.abhao.icu/v1/common/pcupdater/"
   }],
   mac: {
     extendInfo: {
--- packages/tsdaodaobase/src/Components/WKAvatar/index.tsx
+++ packages/tsdaodaobase/src/Components/WKAvatar/index.tsx
@@ -52,11 +52,11 @@ export default class WKAvatar extends Component<WKAvatarProps, WKAvatarState> {
         }
         console.log(imgSrc);
         if (imgSrc.includes("https://43.133.38.23:9000/")){
-            imgSrc = imgSrc.replace("https://43.133.38.23:9000","https://abhao.icu/img")
+            imgSrc = imgSrc.replace("https://43.133.38.23:9000","https://bao.abhao.icu/img")
             console.log(imgSrc);
         }
-        if(!imgSrc.includes("https://abhao.icu")){
-            imgSrc = "https://abhao.icu"+imgSrc;
+        if(!imgSrc.includes("https://bao.abhao.icu")){
+            imgSrc = "https://bao.abhao.icu"+imgSrc;
         }
         // console.log("头像信息"+imgSrc);
         return imgSrc
