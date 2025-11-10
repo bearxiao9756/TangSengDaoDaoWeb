dbf124db157f741251d0dc4ff9de348dcf5a3d9a
bearxiao9756 <bearxiao9@gmail.com>
2025-11-10 21:12:20 +0800

    更新域名

--- packages/tsdaodaodatasource/src/task.ts
+++ packages/tsdaodaodatasource/src/task.ts
@@ -76,11 +76,11 @@ export class MediaMessageUploadTask extends MessageTask {
        const result = await WKApp.apiClient.get(`file/upload?path=${path}&type=chat`)
        if(result) {
           var uploadurl = result.url
-          if(uploadurl.includes('http://bao.abhao.icu:8090/')){
-            uploadurl = uploadurl.replace('http://bao.abhao.icu:8090', 'https://bao.abhao.icu/api')
+          if(uploadurl.includes('http://hao.aehao.icu:8090/')){
+            uploadurl = uploadurl.replace('http://hao.aehao.icu:8090', 'https://hao.aehao.icu/api')
           }
-          if(uploadurl.includes('https://bao.abhao.icu:8090/')){
-            uploadurl = uploadurl.replace('https://bao.abhao.icu:8090', 'https://bao.abhao.icu/api')
+          if(uploadurl.includes('https://hao.aehao.icu:8090/')){
+            uploadurl = uploadurl.replace('https://hao.aehao.icu:8090', 'https://hao.aehao.icu/api')
           }
         //    return result.url
         console.log("上传路径="+uploadurl);
