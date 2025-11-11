6e8089e86736d2a3c71eb993d46bd8d23b51e7d4
bearxiao9756 <bearxiao9@gmail.com>
2025-11-11 18:17:34 +0800

    更新

--- apps/web/electron-builder.js
+++ apps/web/electron-builder.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   publish: [{
     provider: "generic",
-    url: "https://bao.aehao.icu/v1/common/pcupdater/"
+    url: "https://hao.aehao.icu/v1/common/pcupdater/"
   }],
   mac: {
     extendInfo: {
