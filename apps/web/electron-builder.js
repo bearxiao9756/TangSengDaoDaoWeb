d9b17b035dc198e3a4fb6ae10717e7954b046b1e
bearxiao9756 <bearxiao9@gmail.com>
2025-11-13 18:39:52 +0800

    更新域名

--- apps/web/electron-builder.js
+++ apps/web/electron-builder.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   publish: [{
     provider: "generic",
-    url: "https://c.imhao.icu/api/v1/common/pcupdater/"
+    url: "https://ang.aehao.icu/api/v1/common/pcupdater/"
   }],
   mac: {
     extendInfo: {
--- apps/web/src-election/main/confing.ts
+++ apps/web/src-election/main/confing.ts
@@ -1,7 +1,7 @@
 const TSDD_FONFIG = {
   appId: "com.tsdaodao.im",
   name: "广安服务",
-  updataUrl: 'https://c.imhao.icu/api/'
+  updataUrl: 'https://ang.aehao.icu/api/'
 };
 
 export default TSDD_FONFIG;
