09b8f6eb9888250aa387097606ba5fd054b60b42
bearxiao9756 <bearxiao9@gmail.com>
2025-11-20 09:29:34 +0800

    更新为上海华年

--- apps/web/electron-builder.js
+++ apps/web/electron-builder.js
@@ -69,7 +69,7 @@ module.exports = {
     // installerHeaderIcon: "./build/icon.ico", // 安装时头部图标
     createDesktopShortcut: true, // 创建桌面图标
     createStartMenuShortcut: true, // 创建开始菜单图标
-    shortcutName: "广安服务", // 图标名称
+    shortcutName: "上海华年", // 图标名称
   },
   linux: {
     target: ["AppImage", "deb"],
