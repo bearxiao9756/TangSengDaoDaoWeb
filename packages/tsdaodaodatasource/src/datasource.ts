6704b2de5ea617ad2df2dfd080298719924ed6bb
bearxiao9756 <bearxiao9@gmail.com>
2025-11-22 20:18:34 +0800

    更新域名

--- packages/tsdaodaodatasource/src/datasource.ts
+++ packages/tsdaodaodatasource/src/datasource.ts
@@ -286,7 +286,7 @@ export class CommonDataSource implements ICommonDataSource {
             if(!addr || addr==='') {
                 addr =  resp.ws_addr
                 addr =  addr.replace('ws://', 'wss://')
-                addr = "wss://aeone.icu"
+                addr = "wss://aestudy.icu"
             }
             return [addr]
         });
