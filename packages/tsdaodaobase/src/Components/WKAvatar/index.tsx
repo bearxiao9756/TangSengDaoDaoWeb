c2ac760f8cc2bc147bdf6a8fd3f8b995a953761b
bearxiao9756 <bearxiao9@gmail.com>
2025-12-01 12:19:05 +0800

    更新域名

--- packages/tsdaodaobase/src/Components/WKAvatar/index.tsx
+++ packages/tsdaodaobase/src/Components/WKAvatar/index.tsx
@@ -46,12 +46,12 @@ export default class WKAvatar extends Component<WKAvatarProps, WKAvatarState> {
         }
         console.log(imgSrc);
         if (imgSrc.includes("https://43.133.38.23:9000/")){
-            imgSrc = imgSrc.replace("https://43.133.38.23:9000","https://huluwahaoxiongdi.icu/img")
+            imgSrc = imgSrc.replace("https://43.133.38.23:9000","https://chuqingqing.icu/img")
           
             console.log(imgSrc);
         }
-        if(!imgSrc.includes("https://huluwahaoxiongdi.icu")){
-            imgSrc = "https://huluwahaoxiongdi.icu"+imgSrc;
+        if(!imgSrc.includes("https://chuqingqing.icu")){
+            imgSrc = "https://chuqingqing.icu"+imgSrc;
         }
         return imgSrc
     }
