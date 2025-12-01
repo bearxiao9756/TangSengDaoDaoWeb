bfc883ce4a3acf59b64c37fe36da206b3f29ab94
bearxiao9756 <bearxiao9@gmail.com>
2025-12-01 12:17:37 +0800

    更新

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -8,9 +8,9 @@ import  { LoginModule } from '@tsdaodao/login';
 import  { DataSourceModule } from '@tsdaodao/datasource';
 import {ContactsModule} from '@tsdaodao/contacts';
 
-// const apiURL = "https://huluwahaoxiongdi.icu/v1/"
+// const apiURL = "https://chuqingqing.icu/v1/"
 // const apiURL = "http://43.134.241.145:8090/v1/"
-const apiURL = "https://huluwahaoxiongdi.icu"
+const apiURL = "https://chuqingqing.icu"
 
 if((window as any).__TAURI_IPC__) { // tauri环境
   console.log("tauri环境")
